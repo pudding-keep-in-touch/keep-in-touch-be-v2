549a39ff16a79e43f44f052bf7423b9014307dff
nibble2 <vanillaleedana@gmail.com>
2024-08-27 16:03:17 +0900

    feat messages 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { HealthModule } from '@modules/health/health.module';
+import { CommonModule } from '@common/common.module';
+import { MessagesModule } from '@modules/v1/messages/messages.module';
 @Module({
-  imports: [],
+  imports: [HealthModule, CommonModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
