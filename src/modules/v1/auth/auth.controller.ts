ca618f136c17cf697877107f19811724260424dc
nibble2 <vanillaleedana@gmail.com>
2024-08-29 14:27:08 +0900

    🎨 auth 모듈 추가

---
+++ src/modules/v1/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, HttpCode, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+// import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  // constructor(private authService: AuthService) {}
+
+  @Get('kakao') // 카카오 서버를 거쳐서 도착하게 될 엔드포인트
+  @UseGuards(AuthGuard('kakao')) // kakao.strategy를 실행시켜 줍니다.
+  @HttpCode(301)
+  async kakaoLogin(@Req() req: Request) {
+    console.log(req);
+    // 카카오 로그인 리다이렉트
+  }
+}
--- src/router/router.module.ts
+++ src/router/router.module.ts
@@ -1,6 +1,8 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { RouterModule as NestRouterModule } from '@nestjs/core';
 import { MessagesModule } from '@modules/v1/messages/messages.module';
+import { AuthModule } from '@modules/v1/auth/auth.module';
+import { UsersModule } from '@modules/v1/users/users.module';
 
 @Module({})
 export class RouterModule {
@@ -9,10 +11,19 @@ export class RouterModule {
       module: RouterModule,
       imports: [
         MessagesModule,
+        AuthModule,
         NestRouterModule.register([
           {
             path: '/v1',
             module: MessagesModule,
+          },
+          {
+            path: '/v1',
+            module: AuthModule,
+          },
+          {
+            path: '/v1',
+            module: UsersModule,
           },
         ]),
       ],
