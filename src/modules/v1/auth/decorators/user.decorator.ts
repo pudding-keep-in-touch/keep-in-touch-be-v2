d10caf170a61184cff46ef402070b02d249497c7
nibble2 <vanillaleedana@gmail.com>
2024-09-12 01:05:49 +0900

    🎨 auth 데코레이터 이동

--- src/modules/v1/auth/decorators/user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-// Auth 데코레이터
-export const UserAuth = createParamDecorator((_: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user;
-});
