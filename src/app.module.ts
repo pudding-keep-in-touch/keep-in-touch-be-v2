d5769e74e65e7de503c0b002b3085332e80e8c83
Yeshin Lee <yeslee5004@gmail.com>
2024-09-13 19:52:51 +0900

    :bug: Update Jest moduleNameMapper to resolve parent directory reference issue
     Could not locate module ~ mapped as

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HealthModule } from '@modules/health/health.module';
 import { CommonModule } from '@common/common.module';
 import { RouterModule } from '@router/router.module';
-import { DirectMessagesModule } from './modules/v1/direct-messages/direct-messages.module';
+import { DirectMessagesModule } from '@modules/v1/direct-messages/direct-messages.module';
+import { HealthModule } from '@health/health.module';
 
 @Module({
-  imports: [HealthModule, CommonModule, RouterModule.register(), DirectMessagesModule],
+  imports: [CommonModule, HealthModule, RouterModule.register(), DirectMessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
