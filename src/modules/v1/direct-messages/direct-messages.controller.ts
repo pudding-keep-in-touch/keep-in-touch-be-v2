a37e03d3355f98159b2001e99bfb199aad251b9f
Yeshin Lee <yeslee5004@gmail.com>
2024-09-14 00:59:15 +0900

    :bug: Modify request decorator and add custom decorator

--- src/modules/v1/direct-messages/direct-messages.controller.ts
+++ src/modules/v1/direct-messages/direct-messages.controller.ts
@@ -1,6 +1,6 @@
 import { DirectMessagesService } from './direct-messages.service';
 import { GenerateSwaggerApiDoc, NotUserAuth } from '@common/common.decorator';
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RequestGetDmDetailDto } from './dtos/get-dm-detail.dto';
 import { response } from '@common/helpers/common.helper';
@@ -23,11 +23,12 @@ export class DirectMessagesController {
   }
 
   @Post()
+  @NotUserAuth()
   @GenerateSwaggerApiDoc({
     summary: '쪽지 전송',
     description: '받는 사람 가입 email로 쪽지 보내기',
   })
-  async createDm(@Query() createDmDto: CreateDmDto) {
+  async createDm(@Body() createDmDto: CreateDmDto) {
     const result = await this.directMessagesService.createDm(createDmDto);
 
     return response(result, '쪽지 전송 성공');
