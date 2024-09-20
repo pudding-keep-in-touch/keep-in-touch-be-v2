4ac04d5475ad4ce3d0829c49f8051f9101869620
nibble2 <vanillaleedana@gmail.com>
2024-09-20 14:49:09 +0900

    💄 env config 수정

--- src/configs/process-env.config.ts
+++ src/configs/process-env.config.ts
@@ -1,20 +1,15 @@
 import * as Joi from 'joi';
 
-export function validateEnv() {
-  return Joi.object({
-    APP_NAME: Joi.string().required(),
-    APP_ENV: Joi.valid('local', 'dev', 'prod').default('local').required(),
-
-    POSTGRES_HOST: Joi.string().required(),
-    POSTGRES_PORT: Joi.number().required(),
-    POSTGRES_USERNAME: Joi.string().required(),
-    POSTGRES_PASSWORD: Joi.string().required(),
-    POSTGRES_DATABASE: Joi.string().required(),
-
-    GOOGLE_CLIENT_ID: Joi.string().required(),
-    GOOGLE_CLIENT_SECRET: Joi.string().required(),
-
-    JWT_SECRET: Joi.string().required(),
-    JWT_EXPIRES_IN: Joi.string().required(),
-  });
-}
+export const validateEnv = Joi.object({
+  APP_NAME: Joi.string().required(),
+  APP_ENV: Joi.string().required(),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().required(),
+  POSTGRES_USERNAME: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DATABASE: Joi.string().required(),
+  GOOGLE_CLIENT_ID: Joi.string().required(),
+  GOOGLE_CLIENT_SECRET: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.string().required(),
+});
