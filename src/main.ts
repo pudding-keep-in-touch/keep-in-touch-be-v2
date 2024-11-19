ec4e38331aa87f91e0422dd96e2ef4ab398cf1d2
Jisu <hellojs242@gmail.com>
2024-11-19 16:19:12 +0900

    :sparkles: feat: swagger 버전을 v1/v2로 변경
    - api/v1, api/v2로 분리

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { AllExceptionsFilter } from '@common/filters/all-exception.filter';
-import { LoggingInterceptor } from '@common/interceptors/logging.interceptor';
-import { CustomLogger } from '@logger/custom-logger.service';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ConfigModule } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AllExceptionsFilter } from '@common/filters/all-exception.filter';
+import { LoggingInterceptor } from '@common/interceptors/logging.interceptor';
+import { CustomLogger } from '@logger/custom-logger.service';
+import { AuthModule as AuthV1Module } from '@v1/auth/auth.module';
+import { DirectMessagesModule as DirectV1MessagesModule } from '@v1/direct-messages/direct-messages.module';
+import { UsersModule as UsersV1Module } from '@v1/users/users.module';
+import { AuthModule as AuthV2Module } from '@v2/auth/auth.module';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -15,10 +20,10 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const logger = app.get(CustomLogger);
-  //app.useLogger(logger);
+  app.useLogger(logger);
   // 환경 변수 로드
   const environment = configService.get('APP_ENV') || 'development';
-  logger.log(`Application is running in ${environment} mode`);
+  logger.log(`Application is running in ${environment} mode`, 'Bootstrap');
 
   //FIXME: 더 안전한 CORS 설정 필요
   app.enableCors();
@@ -37,12 +42,9 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter(logger));
   app.useGlobalInterceptors(new LoggingInterceptor(logger));
 
-  // swagger 설정
-  const config = new DocumentBuilder()
-    .setTitle(`${configService.get('APP_NAME')}_${configService.get('APP_ENV')}`)
-    .setDescription('API description')
-    // .setVersion('1.0')
-    // .addTag('your-tag')
+  const v1Options = new DocumentBuilder()
+    .setTitle('API V1')
+    .setVersion('1.0')
     .addBearerAuth(
       {
         type: 'http',
@@ -53,8 +55,31 @@ async function bootstrap() {
       'jwt',
     )
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  const v2Options = new DocumentBuilder()
+    .setTitle('API v2')
+    .setVersion('2.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'jwt',
+        in: 'header',
+      },
+      'jwt',
+    )
+    .build();
+
+  const v1Document = SwaggerModule.createDocument(app, v1Options, {
+    include: [UsersV1Module, AuthV1Module, DirectV1MessagesModule],
+  });
+
+  const v2Document = SwaggerModule.createDocument(app, v2Options, {
+    include: [AuthV2Module],
+  });
+
+  SwaggerModule.setup('api/v1', app, v1Document);
+  SwaggerModule.setup('api/v2', app, v2Document);
 
   await app.listen(configService.get('APP_PORT') || 3000);
 }
