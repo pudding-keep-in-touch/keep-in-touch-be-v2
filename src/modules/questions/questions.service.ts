a85ebc0d50c456cbce9aa332df39a406aca434e5
Jisu <hellojs242@gmail.com>
2024-11-25 00:44:42 +0900

    :recycle: refactor: createQuestion parameter type 정의
    - question repository의 createQuestion parmeter type 정의
    - 매개변수 순서를 잘못 입력하는 등의  휴먼에러를 줄이기 위해 타입 설정

--- src/modules/questions/questions.service.ts
+++ src/modules/questions/questions.service.ts
@@ -12,10 +12,14 @@ export class QuestionsService {
 
     const questionCount = await this.questionRepository.countQuestionsByUserId(userId);
     if (questionCount >= QUESTION_COUNT_LIMIT) {
-      throw new ConflictException('질문은 10개까지만 생성할 수 있습니다.');
+      throw new ConflictException(`질문은 ${QUESTION_COUNT_LIMIT}개까지만 생성할 수 있습니다.`);
     }
 
-    const questionId = await this.questionRepository.createQuestion(content, isHidden, userId);
+    const questionId = await this.questionRepository.createQuestion({
+      userId: userId,
+      content: content,
+      isHidden: isHidden,
+    });
     return { questionId };
   }
 }
--- src/modules/questions/repository/question.repository.ts
+++ src/modules/questions/repository/question.repository.ts
@@ -3,16 +3,21 @@ import { Repository } from 'typeorm';
 import { CustomEntityRepository } from '@common/custom-typeorm/custom-typeorm.decorator';
 import { Question } from '@entities/question.entity';
 
+type CreateQuestionParams = {
+  userId: string;
+  content: string;
+  isHidden: boolean;
+};
+
 @CustomEntityRepository(Question)
 export class QuestionRepository extends Repository<Question> {
   /**
    * 질문을 생성하고 생성된 질문의 id를 반환합니다.
-   *
-   * @param content
-   * @param userId
+   * @param params 질문 생성에 필요한 정보, userId, content, isHidden
    * @returns
    */
-  async createQuestion(content: string, isHidden: boolean, userId: string): Promise<string> {
+  async createQuestion(params: CreateQuestionParams): Promise<string> {
+    const { content, isHidden, userId } = params;
     const result = await this.insert({ content, isHidden, userId });
     return result.identifiers[0].questionId;
   }
