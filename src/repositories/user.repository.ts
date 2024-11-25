9846d79a8fa202777688dab6660a399f1598844b
Jisu <hellojs242@gmail.com>
2024-11-25 16:29:52 +0900

    :sparkles: feat: user 생성 시 message statistics 생성 #22
    -  message 생성 시점보다 user 생성 시점이 더 적은 리소스로 생성 가능하다고 판단

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository } from 'typeorm';
 
 import { CustomEntityRepository } from '@common/custom-typeorm/custom-typeorm.decorator';
+import { MessageStatistic } from '@entities/message-statistic.entity';
 import { User } from '@entities/user.entity';
 
 @CustomEntityRepository(User)
@@ -14,8 +15,23 @@ export class UserRepository extends Repository<User> {
    * @returns 생성한 user의 userId
    */
   async createUser(email: string, nickname: string, loginType: number): Promise<string> {
-    const result = await this.insert({ email, nickname, loginType });
-    return result.identifiers[0].userId;
+    return this.manager.transaction(async (transactionalEntityManager) => {
+      // 1. User 생성
+      const userResult = await transactionalEntityManager.insert(User, {
+        email,
+        nickname,
+        loginType,
+      });
+
+      const userId = userResult.identifiers[0].userId;
+
+      // 2. MessageStatistics 생성 (user와 1:1 관계)
+      await transactionalEntityManager.insert(MessageStatistic, {
+        userId: userId,
+      });
+
+      return userId;
+    });
   }
 
   // 유저 이메일 기준으로 조회
