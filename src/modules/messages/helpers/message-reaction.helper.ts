7b9ea3a4de1cb97e148de3160ee10049bc638a8a
Jisu <hellojs242@gmail.com>
2024-12-10 15:53:06 +0900

    :hammer: chore: message status 관련 매핑 jsdoc 추가 #42

--- src/modules/messages/helpers/message-reaction.helper.ts
+++ src/modules/messages/helpers/message-reaction.helper.ts
@@ -3,12 +3,20 @@ import { MessageStatus } from '@entities/message.entity';
 import { ReactionTemplateType } from '@entities/reaction-template.entity';
 import { MessageStatusString, ReactionTypeKorean } from '../types/messages.type';
 
+/**
+ * 문자열 상태값을 MessageStatus enum으로 변환하기 위한 매핑
+ * @example MESSAGE_STATUS_MAPPING['normal'] === MessageStatus.NORMAL
+ */
 const MESSAGE_STATUS_MAPPING = {
   normal: MessageStatus.NORMAL,
   hidden: MessageStatus.HIDDEN,
   reported: MessageStatus.REPORTED,
 } as const;
 
+/**
+ * MessageStatus enum을 문자열 상태값으로 변환하기 위한 매핑
+ * @example REVERSE_STATUS_MAPPING[MessageStatus.NORMAL] === 'normal'
+ */
 const REVERSE_STATUS_MAPPING = {
   [MessageStatus.NORMAL]: 'normal',
   [MessageStatus.HIDDEN]: 'hidden',
