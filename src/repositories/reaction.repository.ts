c7800ba291c2f37b3f8e61c3142049caeea0133c
Jisu <hellojs242@gmail.com>
2024-12-19 00:41:56 +0900

    :sparkles: feat: Reaction 생성 시 ReactionInfo도 생성 #44

--- src/repositories/reaction.repository.ts
+++ src/repositories/reaction.repository.ts
@@ -1,10 +1,11 @@
 import { CustomEntityRepository } from '@common/custom-typeorm/custom-typeorm.decorator';
+import { ReactionInfo } from '@entities/reaction-info.entity';
 import { Reaction } from '@entities/reaction.entity';
+import { ConflictException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 
 @CustomEntityRepository(Reaction)
 export class ReactionRepository extends Repository<Reaction> {
-  //
   async createReactionsToMessage(messageId: string, reactionTemplateIds: string[]): Promise<string[]> {
     const reactions = reactionTemplateIds.map((reactionTemplateId) => {
       return this.create({
@@ -13,8 +14,14 @@ export class ReactionRepository extends Repository<Reaction> {
       });
     });
 
-    // bulk insert
-    const result = await this.insert(reactions);
-    return result.identifiers.map((id) => id.reactionId);
+    return this.manager.transaction(async (manager) => {
+      const reactionInfo = await manager.findOne(ReactionInfo, { where: { messageId } });
+      if (reactionInfo) {
+        throw new ConflictException('이미 반응을 추가한 메시지입니다.');
+      }
+      const reaction = await manager.save(Reaction, reactions);
+      await manager.insert(ReactionInfo, { messageId, createdAt: reaction[0].createdAt }); // reaction의 createdAt을 사용
+      return reaction.map((r) => r.reactionId);
+    });
   }
 }
